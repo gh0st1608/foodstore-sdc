4998f75ae4ec0d6b35a364c759115c7ca4a4dc92
gh0st1608 <egalindoa@uni.pe>
2025-07-27 19:47:22 -0500

    fix .ENVS

--- backend/auth/src/infrastructure/repository/user.repository.impl.ts
+++ backend/auth/src/infrastructure/repository/user.repository.impl.ts
@@ -17,8 +17,8 @@ export class UserRepositoryImpl implements UserRepository {
     const client = new DynamoDBClient({
       region: process.env.AWS_REGION || 'us-east-1',
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        accessKeyId: process.env.ACCESS_KEY_ID!,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY!,
       },
     });
 
