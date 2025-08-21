95f029c00d672fa80c4dee917a697fcdffacd531
gh0st1608 <egalindoa@uni.pe>
2025-08-21 01:13:38 -0500

    fix

--- backend/order/src/main.local.ts
+++ backend/order/src/main.local.ts
@@ -5,7 +5,6 @@ import { OrdersModule } from './order.module';
 async function start() {
   const app = await bootstrap(BootstrapMode.Http, OrdersModule);
   await app.listen(process.env.PORT);
-  console.log(`🚀 Server running on http://localhost:3002`);
 }
 
 start();
